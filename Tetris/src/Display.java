a44f51d3fac55b67eef54268c174ca5c2d5bfab3
Rafi Long <rafilong42@gmail.com>
2014-02-24 12:05:46 -0800

    Added background image variables to code

--- Tetris/src/Display.java
+++ Tetris/src/Display.java
@@ -1,10 +1,22 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 
 @SuppressWarnings("serial")
 public class Display extends JComponent {
-
+	BufferedImage background;
+	
+	
 	public Display() {
-		
+		try {
+			background = ImageIO.read(new File("Board.png"));
+		} catch (IOException e) {
+			System.out.println("Background image does not exist");
+			e.printStackTrace();
+		}
 	}
 }
